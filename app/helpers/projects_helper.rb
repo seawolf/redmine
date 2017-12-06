e4ea86bff068cafefda6bbbb281afa93f6edf735
Ben Arnold <ben@seawolfsanctuary.com>
2017-12-06 15:58:45 +0000

    Add: link to project's wiki if enabled, on project list page

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -58,7 +58,7 @@ module ProjectsHelper
   # Renders the projects index
   def render_project_hierarchy(projects)
     render_project_nested_lists(projects) do |project|
-      s = link_to_project(project, {}, :class => "#{project.css_classes} #{User.current.member_of?(project) ? 'icon icon-fav my-project' : nil}")
+      s = link_to_project(project, {}, { class: "#{project.css_classes} #{User.current.member_of?(project) ? 'icon icon-fav my-project' : nil}" }, true)
       if project.description.present?
         s << content_tag('div', textilizable(project.short_description, :project => project), :class => 'wiki description')
       end
